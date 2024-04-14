393ad9ef48191582660f972bbfd763232eb1da24
Foresi9ht <157278731+Foresi9ht@users.noreply.github.com>
2024-04-14 17:54:29 +0600

    Delete Lab5/regex directory

--- Lab5/regex/5.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def ch(t):
-    pattern = re.compile(r'a[a-z]*b$')
-    return [m.group(0) for i in t for m in [pattern.match(i)] if m]
-
-t = ["ab", 'aucnrb', ',cjrnfn', 'aknfvhbd','ahgjesfdmbncj']
-res = ch(t)
-print(res)
